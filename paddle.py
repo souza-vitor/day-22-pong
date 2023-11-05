79c38839cb3ea08d80ae861124bcfdba9e9c891d
Vitor Souza <91034011+souza-vitor@users.noreply.github.com>
2023-11-05 10:27:53 -0300

    Add files via upload

---
+++ paddle.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.color("white")
+        self.shape("square")
+        self.shapesize(5, 1)
+        self.penup()
+        self.setposition(position)
+
+    def go_up(self):
+        self.goto(self.xcor(), self.ycor() + 20)
+
+    def go_down(self):
+        self.goto(self.xcor(), self.ycor() - 20)
